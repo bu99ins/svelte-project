ef8402462fb10bda8338d9fb745e86278c33d699
Yury Krasavin <buggins@live.com>
2019-07-01 20:09:05 +0300

    add TextInput component

---
+++ src/UI/TextInut.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let controlType;
+  export let id;
+</script>
+
+<div class="form-control">
+  <label for={id}>Description</label>
+  {#if controlType === 'textarea'}
+    <textarea rows="3" {id} binnd:value={description} />
+  {:else}
+    <input type="text" {id} binnd:value={title} />
+  {/if}
+</div>
